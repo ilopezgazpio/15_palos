7c4f458e0d26177d9566b4737a43e7dd0f37a469
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2019-10-22 12:48:18 +0200

    Minor changes - tab 4 spaces + python 2 and 3 support to read input

--- human_agent.py
+++ human_agent.py
@@ -1,25 +1,32 @@
+import sys
+
 class Human:
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def set_player1(self):
-    self.player = 1
+    def set_player1(self):
+        self.player = 1
 
-  def set_player2(self):
-    self.player = 2
+    def set_player2(self):
+        self.player = 2
 
-  def take_action(self, env):
-    while True:
-      move = input("\nEnter level [0, 1, 2] and number of pieces to take from board separated by space (e.g. 0 3): ")
-      level, number  = move.split(' ')
-      move = ( int(level), int(number) )
+    def take_action(self, env):
+        while True:
+            move = ""
+            if sys.version_info[0] > 3:
+                move = input("\nEnter level [0, 1, 2] and number of pieces to take from board separated by space (e.g. 0 3): ")
+            else:
+                move = raw_input("\nEnter level [0, 1, 2] and number of pieces to take from board separated by space (e.g. 0 3): ")
 
-      if env.correct_move(move):
-        env.make_move(move)
-        break
+            level, number = move.split(' ')
+            move = (int(level), int(number))
 
-  def update(self, env):
-    pass
+            if env.correct_move(move):
+                env.make_move(move)
+                break
 
-  def update_state_history(self, s):
-    pass
+    def update(self, env):
+        pass
+
+    def update_state_history(self, s):
+        pass
