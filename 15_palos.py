01e63e0e77ba40f3475868e8a56eb8c7ecce822a
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2019-10-22 12:56:30 +0200

    Minor changes related to spacing and python 2/3 support

--- 15_palos.py
+++ 15_palos.py
@@ -5,6 +5,7 @@ from agent import Agent
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import sys
 
 
 '''
@@ -13,64 +14,70 @@ Train and play game
 
 if __name__ == '__main__':
 
-  parser = argparse.ArgumentParser(description='Generate data.')
-  parser.add_argument('--episodes', type=int, default=30000, help='Number of episodes to train the model')
-  parser.add_argument('--alpha', type=float, default=0.05, help='Number of episodes to train the model')
-  parser.add_argument('--epsilon', type=float, default=0.15, help='Number of episodes to train the model')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Generate data.')
+    parser.add_argument('--episodes', type=int, default=30000, help='Number of episodes to train the model')
+    parser.add_argument('--alpha', type=float, default=0.05, help='Number of episodes to train the model')
+    parser.add_argument('--epsilon', type=float, default=0.15, help='Number of episodes to train the model')
+    args = parser.parse_args()
 
-  env = Environment()
+    env = Environment()
 
-  player1 = Agent(args.epsilon, args.alpha)
-  player1.set_player1()
-  player1.setV(env.initial_values())
+    player1 = Agent(args.epsilon, args.alpha)
+    player1.set_player1()
+    player1.setV(env.initial_values())
 
-  player2 = Agent(args.epsilon, args.alpha)
-  player2.set_player2()
-  player2.setV(env.initial_values())
+    player2 = Agent(args.epsilon, args.alpha)
+    player2.set_player2()
+    player2.setV(env.initial_values())
 
-  players = [player1, player2]
+    players = [player1, player2]
 
-  print("=================")
-  print("Starting training")
-  print("=================")
-  for e in range(args.episodes):
-    if e % 5000 == 0:
-      print(e)
-    order = np.random.permutation( range(len(players)) )
-    env.play_game( players[order[0]], players[order[1]] )
-  print("============")
-  print("End training")
-  print("============")
+    print("=================")
+    print("Starting training")
+    print("=================")
+    for e in range(args.episodes):
+        if e % 5000 == 0:
+            print(e)
+        order = np.random.permutation( range(len(players)) )
+        env.play_game( players[order[0]], players[order[1]] )
 
-  human = Human()
-  rounds = 0
+    print("============")
+    print("End training")
+    print("============")
 
-  while True:
-    print("Starting round {}".format(rounds))
+    human = Human()
+    rounds = 0
 
-    if rounds % 2 == 0:
-      aiPlayer = player1
-      aiPlayer.set_verbose(True)
-      aiPlayer.eps = 0
-      aiPlayer.set_player1()
-      human.set_player2()
-      env.play_game(aiPlayer, human, draw=2)
-    else:
-      aiPlayer = player2
-      aiPlayer.set_verbose(True)
-      aiPlayer.eps = 0
-      aiPlayer.set_player2()
-      human.set_player1()
-      env.play_game(human, aiPlayer, draw=1)
+    while True:
+        print("Starting round {}".format(rounds))
 
-    if env.winner == aiPlayer.player:
-      print("\n Game Over \n")
-    elif env.winner == human.player:
-      print("\n Victory \n")
+        if rounds % 2 == 0:
+            aiPlayer = player1
+            aiPlayer.set_verbose(True)
+            aiPlayer.eps = 0
+            aiPlayer.set_player1()
+            human.set_player2()
+            env.play_game(aiPlayer, human, draw=2)
+        else:
+            aiPlayer = player2
+            aiPlayer.set_verbose(True)
+            aiPlayer.eps = 0
+            aiPlayer.set_player2()
+            human.set_player1()
+            env.play_game(human, aiPlayer, draw=1)
 
-    rounds += 1
-    answer = input("Play again? [Y/n]: ")
-    if answer and answer.lower()[0] == 'n':
-      break
+        if env.winner == aiPlayer.player:
+            print("\n Game Over \n")
+        elif env.winner == human.player:
+            print("\n Victory \n")
+
+        rounds += 1
+
+        if sys.version_info[0] > 3:
+            answer = input("Play again? [Y/n]: ")
+        else:
+            answer = raw_input("Play again? [Y/n]: ")
+
+        if answer and answer.lower()[0] == 'n':
+            break
 
