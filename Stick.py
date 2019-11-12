bd1783d4af078a711a563b174d0ac5fba30b44bc
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2019-11-12 11:52:26 +0100

    Add classes to handle sticks

---
+++ Stick.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Stick(pygame.sprite.Sprite):
+
+    def __init__(self, location):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image_icon = 'resources/stick.png'
+        self.x = location[0]
+        self.y = location[1]
+
+        self.__load_image()
+
+    def __load_image(self):
+        # load image
+        self.image = pygame.image.load(self.image_icon)
+
+        # set rectangle
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.x, self.y)
+        self.rect.width = self.image.get_rect().width
+        self.rect.height = self.image.get_rect().height
+
+    def clicked(self):
+        if not self.isSelected():
+            self.image_icon = 'resources/stick_selected.png'
+            self.__load_image()
+        else:
+            self.image_icon = 'resources/stick.png'
+            self.__load_image()
+
+    def printCoord(self):
+        print("({},{})".format(self.x, self.y))
+
+    def printRect(self):
+        print("From ({},{}) , to ({},{})".format(self.x, self.y, self.x + self.rect.width, self.y + self.rect.width))
+
+    def isSelected(self):
+        return self.image_icon == 'resources/stick_selected.png'
+
+
---
+++ Sticks.py
@@ -0,0 +1,28 @@
+import pygame
+from Stick import Stick
+
+class Sticks(pygame.sprite.Group):
+
+    def __init__(self):
+        pygame.sprite.Group.__init__(self)
+
+        # BadAss hardcoded for 720 resolution
+        size = (3, 5, 7)
+        delta_x = [215 , 150, 90]
+        delta_space = (40, 40, 40)
+        y = 5
+
+        for i in range(len(size)):
+            level = size[i]
+
+            for j in range(level):
+                stick = Stick((delta_x[i], y))
+                self.add(stick)
+                delta_x[i] += stick.rect.width + delta_space[i]
+
+            y += stick.rect.height + 5
+
+    def removeSelected(self):
+        for stick in self.sprites():
+            if stick.isSelected():
+                stick.kill()
