fef266be3bfbb1b9cb04e4cd1e24a9df30d4b8eb
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2019-11-26 16:17:47 +0100

    added game over scene

---
+++ GameOverScene.py
@@ -0,0 +1,28 @@
+from Scene import Scene
+import pygame
+import time
+import utils
+
+class GameOverScene(Scene):
+
+    def __init__(self, message, args = {}):
+        '''Initialize'''
+        Scene.__init__(self, args)
+
+    def handle_events(self, events):
+        self.next_scene = None
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_y:
+                self.next_scene = 'GameScene'
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
+                self.showing = False
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        screen.fill(utils.BLACK)
+        dimension = screen.get_size()
+        (surface, rectangle) = utils.get_surfrect_text('Play again ?', 'arial', 55, utils.RED, dimension[0] / 2, dimension[1] / 2)
+        screen.blit(surface, rectangle)
+        #self.showing = False
